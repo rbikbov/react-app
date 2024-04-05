093e55ba2717424d1f73a4e8ce9aeba343e9025e
Rafis Bikbov <rafis.bikbov@gmail.com>
2024-04-05 03:39:56 +0300

    chore: vite-plugin-checker added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { fileURLToPath, URL } from 'url'
+import { checker } from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // https://vite-plugin-checker.netlify.app/introduction/getting-started.html
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),
+  ],
   resolve: {
     alias: [
       {
